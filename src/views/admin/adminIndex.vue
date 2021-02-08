8ea55c19442b077d87dfb89b2fd95246db052ce3
npuyan <zty2804@163.com>
2021-02-08 22:05:30 +0800

    教师和管理员

---
+++ src/views/admin/adminIndex.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'adminIndex'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/teacher/teacherIndex.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'teacherIndex'
+}
+</script>
+
+<style scoped>
+
+</style>
