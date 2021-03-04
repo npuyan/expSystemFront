04c20dba5419e0c764cad296c15e42871563190d
杨晓峰 <yangxiaofeng.yang@aliyun.com>
2021-03-04 16:55:04 +0800

    fix merge conflicts

--- src/views/student/lay.vue
+++ src/views/student/lay.vue
@@ -9,62 +9,65 @@
         left: 0,
       }"
     >
+      <!--  选课按钮    -->
       <div>
-        <br />
-        <br />
+        <br/>
+        <br/>
         <a-button type="primary" size="large" @click="selectcourse()">
           进入选课
         </a-button>
-        <br />
-        <br />
+        <br/>
+        <br/>
       </div>
-
+      <!--搜索栏-->
       <div class="logo">
         <a-input-search
           aria-placeholder="搜索"
           @click="search"
         ></a-input-search>
       </div>
-
-      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
-        <!--        @click="getlab(courseitem.courseId, courseitem.courseName)">-->
-        <a-menu-item
-          :key="courseitem.courseId"
-          v-for="courseitem in courselist"
-          @click="getlab(courseitem.courseId, courseitem.courseName)"
-        >
-          {{ courseitem.courseName }}
+      <!-- 左侧课程名称-->
+      <a-menu theme="dark"
+              mode="inline"
+              :default-selected-keys="[courselist[0].courseId]"
+              @select="getlab">
+        <a-menu-item v-for="item in courselist" :key="item.courseId">
+          <!--                     @click="getlab(key, courseitem.courseName)"-->
+          <span>  {{ item.courseName }}</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
+
     <a-layout :style="{ marginLeft: '200px' }">
+      <!--     右侧叶面题头 -->
       <a-layout-header style="background: #fff; padding: 0; text-align: center">
         <h1 style="font-size: 25px">实验系统</h1>
       </a-layout-header>
+
       <a-layout-content style="margin: 0 16px">
+
+        <!--    右侧crumb    -->
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>学生</a-breadcrumb-item>
           <a-breadcrumb-item>{{ course_name }}</a-breadcrumb-item>
         </a-breadcrumb>
-        <div
-          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
-        >
+
+        <!--右侧实验列表        -->
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
           <a-list :grid="{ gutter: 16, column: 4 }" :data-source="lablist">
             <a-list-item
               slot="renderItem"
               slot-scope="item, index"
-              @click="onJump('6080')"
-            >
-              <img
-                src="../../assets/logo.png"
-                style="width: 100px; height: 100px"
-              />
-
-              <a-list-item-meta description="容器镜像">
-                <a slot="title">{{ item.labName }}</a>
-              </a-list-item-meta>
+              @click="onJumpLab(item)">
+              <!--          容器显示卡片    -->
+              <a-card>
+                <img src="../../assets/logo.png" style="width: 100px; height: 100px"/>
+                <a-list-item-meta description="容器镜像">
+                  <a slot="title">{{ item.labName }}</a>
+                </a-list-item-meta>
+              </a-card>
             </a-list-item>
-            <br />
+            <br/>
           </a-list>
         </div>
       </a-layout-content>
@@ -78,7 +81,6 @@
 export default {
   data () {
     // todos
-
     return {
       collapsed: true,
       courselist: [],
@@ -86,34 +88,37 @@ export default {
       lablist: []
     }
   },
-
   mounted () {
     console.log('mounted!')
-
     var _this = this
-    _this
-      .postRequest('api/getselectedcourses', {
-        username: String(this.$route.query.user_name)
-      })
-      .then((resp) => {
-        if (resp) {
-          console.log('得到课程数据')
-          console.log(resp)
-          _this.courselist = resp
-        } else {
-          alert('连接服务器失败')
-        }
-      })
+    _this.postRequest('api/getselectedcourses', {
+      username: String(this.$route.query.user_name)
+    }).then((resp) => {
+      if (resp) {
+        console.log('得到课程数据')
+        console.log(resp)
+        this.courselist = resp
+        this.course_name = this.courselist[0].courseName
+        this.getlab({item: 0, key: this.courselist[0].courseId, selectedKeys: 0})
+      } else {
+        alert('连接服务器失败')
+      }
+    })
   },
-
   methods: {
     onJump: function (a) {
       console.log('submit!')
-      // eslint-disable-next-line no-unused-vars
       console.log(this.$router)
-
-      this.$router.push({ path: '/novnc', query: { port: a } })
-      // window.location.assign("http://ubuntu:"+ans)
+      this.$router.push({path: '/novnc', query: {port: a}})
+    },
+    onJumpLab: function (labitem) {
+      console.log(labitem)
+      this.postRequest('', {
+        //  TODO 传入课程，实验，用户名，打开对应的实验环境并返回启动的的端口
+      }).then(
+        //  TODO 跳转到novnc并连接到返回的端口
+      )
+      this.$router.push({path: '/novnc', query: {port: 31399}})
     },
     onJumpNewPort: function (a) {
       console.log('new Port')
@@ -127,19 +132,21 @@ export default {
         .then((resp) => {
           if (resp) {
             alert('连接成功')
-            this.$router.push({ path: '/novnc', query: { port: a } })
+            this.$router.push({path: '/novnc', query: {port: a}})
           } else {
             alert('连接服务器失败')
           }
         })
     },
-
-    search: function () {},
-
-    getlab: function (id, name) {
+    search: function () {
+    },
+    getlab: function ({item, key, selectedKeys}) {
+      console.log('key' + key)
+      var course = this.courselist.filter(it => key === it.courseId)[0]
+      var id = course.courseId
+      var name = course.courseName
       console.log('getlab', id, name)
       this.course_name = name
-      console.log('to string', String(id))
       var _this = this
       _this
         .postRequest('api/getlabbycourseid', {
@@ -155,15 +162,13 @@ export default {
           }
         })
     },
-
     selectcourse: function () {
       console.log('step in select course page!')
       // eslint-disable-next-line no-unused-vars
       console.log(this.$router)
-
       this.$router.push({
         path: '/course',
-        query: { user_name: this.$route.query.user_name }
+        query: {user_name: this.$route.query.user_name}
       })
     }
   }
@@ -171,7 +176,6 @@ export default {
 </script>
 
 <style>
-
 #components-layout-demo-side .logo {
   height: 32px;
   background: rgba(255, 255, 255, 0.2);
