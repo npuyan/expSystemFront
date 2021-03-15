16b92e1c44062db4531d43c8fa859c137d320617
杨晓峰 <yangxiaofeng.yang@aliyun.com>
2021-03-16 01:05:13 +0800

    用pdf.js替换vue-pdf，在static中添加依赖pdf.js-dist

--- src/views/student/novnc.vue
+++ src/views/student/novnc.vue
@@ -20,13 +20,8 @@
             </a-button>
           </a-button-group>
         </div>
-        <div class="pdf">
-          <pdf ref="pdf" 
-            v-for="i in numPages" 
-            :key="i"  
-            :page="i"  
-            :src="pdfUrl"> </pdf>
-        </div>
+        <iframe :src="'/static/pdf/web/viewer.html?file=' + fileUrl" height="820" width="100%">
+        </iframe>
 
       </div>
       <br /><br /><br />
@@ -47,20 +42,16 @@
   </a-layout>
 </template>
 <script>
-import pdf from "vue-pdf";
 
 export default {
-  components: {
-    pdf,
-  },
 
   data() {
     return {
-      pdfUrl: "",
       collapsed: false,
       port: this.$route.query.port,
       url: "http://124.70.84.98:",
       numPages: null, // pdf 总页数 
+      fileUrl: encodeURIComponent('http://localhost:8081/api/downloadfile?filename=' + this.$route.query.doc_path)
     };
   },
   computed: {
@@ -69,15 +60,6 @@ export default {
     },
   },
   methods: {
-    getNumPages() {
-      let loadingTask = pdf.createLoadingTask(this.pdfUrl)
-      loadingTask.promise.then(pdf => {
-        this.numPages = pdf.numPages
-      }).catch(err => {
-        console.error('pdf 加载失败', err);
-      })
-    },
-
     test: function () {
       alert(this.port);
     },
@@ -97,10 +79,6 @@ export default {
         window.onbeforeunload = null;
       }
     };
-
-    var filename = this.$route.query.doc_path;
-    this.pdfUrl = "api/downloadfile?filename=" + filename;
-    this.getNumPages()
   },
 };
 </script>
